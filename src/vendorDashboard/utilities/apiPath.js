b312c6c133c92ed4de723a872b1118cfd99ce013
Jyotendra Kumar Naidu Maripe <jyotendramaripe@Jyotendras-Air.lan>
2024-06-27 11:28:38 +0530

    frontend

--- src/vendorDashboard/utilities/apiPath.js
+++ src/vendorDashboard/utilities/apiPath.js
@@ -1,2 +1,2 @@
-// export const API_URL ="http://localhost:4000";
-export const API_URL ="https://multivendor-backend-762v.onrender.com";
+export const API_URL ="http://localhost:4000";
+// export const API_URL ="https://multivendor-backend-762v.onrender.com";
